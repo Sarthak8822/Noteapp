e0d24457475d00fd7a4f29315a17bc55ed806fa7
Sarthak <sarthakpravin08@gmail.com>
2022-03-14 16:38:49 +0530

    frontend part

--- src/App.js
+++ src/App.js
@@ -1,14 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
+import Search from './components/Search';
+import Header from './components/Header';
+import axios from 'axios';
 
-function App() {
-  return(
-    <div>
-      <NotesList />
-    </div>
-  );
-  
-}
+const App = () => {
+	const [notes, setNotes] = useState([]);
+
+	const [searchText, setSearchText] = useState('');
+
+	const [darkMode, setDarkMode] = useState(false);
+
+	useEffect(() => {
+		// const savedNotes = JSON.parse(
+		// 	localStorage.getItem('react-notes-app-data')
+		// );
+
+		// if (savedNotes) {
+		// 	setNotes(savedNotes);
+		// }
+
+		axios.get('http://localhost:5000/notes')
+		.then(res => {
+			setNotes(res.data);
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem(
+			'react-notes-app-data',
+			JSON.stringify(notes)
+		);
+	}, [notes]);
+
+	const addNote = (text) => {
+		axios.post('http://localhost:5000/notes', {
+			text: text,
+			date: new Date().toLocaleDateString()
+		})
+		.then(res => {
+			setNotes(res.data);
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	};
+
+	const deleteNote = (id) => {
+		axios.delete('http://localhost:5000/notes', {
+			data: {
+				id: id
+			}
+		})
+		.then(res => {
+			setNotes(res.data);
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	};
+
+	return (
+		<div className={`${darkMode && 'dark-mode'}`}>
+			<div className='container'>
+				<Header handleToggleDarkMode={setDarkMode} />
+				<Search handleSearchNote={setSearchText} />
+				<NotesList
+					notes={notes.filter((note) =>
+						note.text.toLowerCase().includes(searchText.toLowerCase())
+					)}
+					handleAddNote={addNote}
+					handleDeleteNote={deleteNote}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,10 +1,25 @@
-import Note from "./Note";
-function NoteList() {
-    return(
-      <div className="note-list">
-        <Note/>
-      </div>
-    );
-    
-  }
-  export default NoteList;
+import Note from './Note';
+import AddNote from './AddNote';
+
+const NotesList = ({
+	notes,
+	handleAddNote,
+	handleDeleteNote,
+}) => {
+	return (
+		<div className='notes-list'>
+			{notes.map((note) => (
+				<Note
+					key={note._id}
+					id={note._id}
+					text={note.text}
+					date={note.date}
+					handleDeleteNote={handleDeleteNote}
+				/>
+			))}
+			<AddNote handleAddNote={handleAddNote} />
+		</div>
+	);
+};
+
+export default NotesList;
